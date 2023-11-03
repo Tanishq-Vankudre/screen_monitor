687f1f3325d2ee4f0f737031b7c8af621b247002
Anish Jarag <111342569+anish-jarag@users.noreply.github.com>
2023-11-03 12:42:21 +0530

    Add files via upload

---
+++ sender.py
@@ -0,0 +1,14 @@
+from vidstream import ScreenShareClient
+import threading
+
+sender = ScreenShareClient('192.168.167.51', 9999)
+
+
+t = threading.Thread(target=sender.start_stream)
+t.start()
+
+while input("") != 'STOP':
+    continue
+
+sender.stop_stream()
+
